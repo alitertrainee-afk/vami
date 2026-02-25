3d96c01d7a2fc684dcc0b740747db1e9039e9b6c
Meet Chauhan <alitertrainee@gmail.com>
2026-02-25 18:24:17 +0530

    [migrate to nuxt from vue]:[development]

--- vami-frontend/src/services/user.service.js
+++
@@ -1,9 +0,0 @@
-// local imports
-import axiosInstance from "../api/axios.instance";
-
-export const UserService = {
-  async searchUsers(query) {
-    if (!query) return { data: [] };
-    return await axiosInstance.get(`/users?search=${query}`);
-  },
-};
